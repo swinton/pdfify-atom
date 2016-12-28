73bd8cde2c236cd0786873d6427eb4f986d2e7fa
Steve Winton <stevewinton@gmail.com>
2016-12-28 13:07:41 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# `pdfify-atom`
+
+Generates Octocat-friendly PDFs from Markdown, within Atom.
+
+Requires [`pdfify`](https://github.com/swinton/pdfify).
+
+## Installation
+
+`TBD`
+
+## Usage
+
+`TBD`
---
+++ spec/pdfify-spec.js
@@ -0,0 +1,18 @@
+'use babel';
+
+import Pdfify from '../lib/pdfify';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Pdfify', () => {
+    let workspaceElement;
+
+    beforeEach(() => {
+        workspaceElement = atom.views.getView(atom.workspace);
+    });
+
+    // TODO
+});
